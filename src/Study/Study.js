3519cdceff241b987de4c773ab41356fba7a7481
Callie Glade <callie.glade@meraki.net>
2022-02-01 14:56:31 -0800

    Add logic to read and set the deck used by Study component

--- src/Study/Study.js
+++ src/Study/Study.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { readDeck } from "../utils/api";
 
 function Study() {
+  const [deck, setDeck] = useState({});
+  const { deckId } = useParams();
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    readDeck(deckId, abortController.signal).then(setDeck);
+    return () => abortController.abort();
+  }, []);
+
   return (
     <p>Study component placeholder</p>
   );
