30441dbb01cd2b3546b52316837fc43bfc413f31
Callie Glade <callie.glade@meraki.net>
2022-02-01 00:56:21 -0800

    Add bootstrap styles to DeckCard

--- src/Layout/DeckCard.js
+++ src/Layout/DeckCard.js
@@ -1,22 +1,40 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 function DeckCard({ name, desc }) {
-
+  const history = useHistory();
   // TODO: Add Flexbox & Boostrap 4 classes to each element
-  
+
   return (
-    <li>
-      <div>
-        <h3>{name}</h3>
-        <p>{/* TODO: number of cards */}</p>
+    <div className="card">
+      <div className="card-body">
+        <div>
+          <h3 className="card-title">{name}</h3>
+          <p>{/* TODO: number of cards */} cards</p>
+        </div>
+        <p className="card-text">{desc}</p>
+        <div>
+          <button 
+            type="button" 
+            className="btn btn-secondary" 
+            onClick={() => history.push(`/decks/:deckId`)} 
+            >View
+          </button>
+          <button 
+            type="button" 
+            className="btn btn-primary" 
+            onClick={() => history.push(`/decks/:deckId/study`)} 
+            >Study
+          </button>
+          <button 
+            type="button" 
+            className="btn btn-primary" 
+            onClick={() => history.push(`/decks/new`)} 
+            >Delete
+          </button>
+        </div>
       </div>
-      <p>{desc}</p>
-      <div>
-        <button>View</button>
-        <button>Study</button>
-        <button>Delete</button>
-      </div>
-    </li>
+    </div>
   );
 }
 
