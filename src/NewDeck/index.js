e99eea72f3c4aec7a3b819889e57d0d9bfcbde2a
Callie Glade <callie.glade@meraki.net>
2022-02-01 21:12:20 -0800

    Add form elements to NewDeck

--- src/NewDeck/index.js
+++ src/NewDeck/index.js
@@ -2,7 +2,35 @@ import React from "react";
 
 function NewDeck() {
   return (
-    <p>NewDeck component placeholder</p>
+    <div>
+      <nav aria-label="breadcrumb">
+        <ol className="breadcrumb">
+          <li className="breadcrumb-item text-primary" onClick={() => history.push(`/`)}>Home</li>
+          <li className="breadcrumb-item active">Create Deck</li>
+        </ol>
+      </nav>
+      <form>
+        <fieldset>
+          <legend>Create Deck</legend>
+          <label for="name">Name</label>
+          <input
+            id="name"
+            type="text"
+            name="name"
+            placeholder="Deck Name"
+            required
+          />
+          <label for="description">Description</label>
+          <textarea
+            id="description"
+            name="description"
+            placeholder="Brief description of the deck"
+            required
+          />
+          <button className="btn btn-primary" type="submit">Submit</button>
+        </fieldset>
+      </form>
+    </div>
   );
 }
 
