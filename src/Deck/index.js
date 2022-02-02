6ddf29efcd9c26f32ff6e926925c7f18f26c2c2d
Callie Glade <callie.glade@meraki.net>
2022-02-02 15:56:41 -0800

    Add Routes to deck view and deck edit view

--- src/Deck/index.js
+++ src/Deck/index.js
@@ -1,10 +1,10 @@
 import React, { useEffect } from "react";
-import { useHistory, useParams } from "react-router-dom";
+import { Switch, Route, useParams } from "react-router-dom";
 import { readDeck } from "../utils/api";
-import Card from "./Card";
+import Deck from "./Deck";
+import Edit from "./Edit";
 
 function DeckLayout({ deck, setDeck }) {
-  const history = useHistory();
   const { deckId } = useParams();
   
   useEffect(() => {
@@ -14,32 +14,16 @@ function DeckLayout({ deck, setDeck }) {
   }, [deckId, setDeck]);
 
   if ( deck.id === 0 ) return <p>Loading...</p>;
-
-  const cardList = deck.cards.map((card) => (
-    <Card key={card.id} card={card} />
-  ));
   
   return (
-    <div>
-      <nav aria-label="breadcrumb">
-        <ol className="breadcrumb">
-          <li className="breadcrumb-item text-primary" onClick={() => history.push(`/`)}>Home</li>
-          <li className="breadcrumb-item active">{deck.name}</li>
-        </ol>
-      </nav>
-      <div>
-        <h3>{deck.name}</h3>
-        <p>{deck.description}</p>
-        <div className="row">
-          <button type="button" className="btn btn-secondary mx-3">Edit</button>
-          <button type="button" className="btn btn-primary mr-3">Study</button>
-          <button type="button" className="btn btn-primary mr-3">+ Add Cards</button>
-          <button type="button" className="btn btn-danger mr-3">Delete</button>
-        </div>
-        <h2 className="my-4">Cards</h2>
-        <div>{cardList}</div>
-      </div>
-    </div>
+    <Switch>
+      <Route exact path={`/decks/:deckId`}>
+        <Deck />
+      </Route>
+      <Route path={`/decks/:deckId/edit`}>
+        <Edit />
+      </Route>
+    </Switch>
   );
 }
 
