2d024f348302aa6cde713f0ebfecf3569c490d80
Callie Glade <callie.glade@meraki.net>
2022-02-01 21:19:51 -0800

    Add Cancel button and bootstrap styles

--- src/NewDeck/index.js
+++ src/NewDeck/index.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 function NewDeck() {
+  const history = useHistory();
+
   return (
     <div>
       <nav aria-label="breadcrumb">
@@ -9,26 +12,33 @@ function NewDeck() {
           <li className="breadcrumb-item active">Create Deck</li>
         </ol>
       </nav>
+      <h2>Create Deck</h2>
       <form>
-        <fieldset>
-          <legend>Create Deck</legend>
+        <div className="form-group">
           <label for="name">Name</label>
           <input
             id="name"
             type="text"
             name="name"
+            className="form-control"
             placeholder="Deck Name"
             required
           />
+        </div>
+        <div className="form-group">
           <label for="description">Description</label>
           <textarea
             id="description"
             name="description"
+            className="form-control"
             placeholder="Brief description of the deck"
             required
           />
-          <button className="btn btn-primary" type="submit">Submit</button>
-        </fieldset>
+        </div>
+        <div className="row">
+          <button type="button" className="btn btn-secondary m-3" onClick={() => history.push(`/`)}>Cancel</button>
+          <button type="submit" className="btn btn-primary my-3">Submit</button>
+        </div>
       </form>
     </div>
   );
