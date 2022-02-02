dad52c3ee572cc77cb4acb46794a7f90db4c68ff
Callie Glade <callie.glade@meraki.net>
2022-02-01 16:39:25 -0800

    Add isFlipped state to allow card to be flipped

--- src/Study/StudyCard.js
+++ src/Study/StudyCard.js
@@ -3,7 +3,8 @@ import { useHistory } from "react-router-dom";
 
 function StudyCard({ deck }) {
   const { id, cards } = deck;
-  const [card, setCard] = useState({id: 0});
+  const [card, setCard] = useState(cards[0]);
+  const [isFlipped, setFlipped] = useState(false);
   const history = useHistory();
 
   if (cards.length <= 2) {
@@ -16,13 +17,17 @@ function StudyCard({ deck }) {
     );
   } else {
     return (
-      /* TODO: Build card displaying functionality */
       <div className="card">
         <div className="card-body">
           <div className="card-title">Card {card.id} of {cards.length}</div>
-          <div className="card-text">{card.front}</div>
+          <div className="card-text">{isFlipped ? card.back : card.front}</div>
           <div className="row">
-            <button type="button" className="btn btn-secondary">Flip</button>
+            <button 
+              type="button"
+              className="btn btn-secondary"
+              onClick={() => setFlipped(!isFlipped)}
+              >Flip
+            </button>
           </div>
         </div>
       </div>
