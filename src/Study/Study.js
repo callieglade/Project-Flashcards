8089034c4312c6c618e0196d2cc7d4e4f19e458a
Callie Glade <callie.glade@meraki.net>
2022-02-01 16:15:35 -0800

    Add conditional rendering for Study and StudyCard

--- src/Study/Study.js
+++ src/Study/Study.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useHistory } from "react-router-dom";
 import { readDeck } from "../utils/api";
+import StudyCard from "./StudyCard";
 
 function Study() {
-  const [deck, setDeck] = useState({});
+  const [deck, setDeck] = useState({id: 0});
   const { deckId } = useParams();
   const history = useHistory();
 
@@ -11,8 +12,9 @@ function Study() {
     const abortController = new AbortController();
     readDeck(deckId, abortController.signal).then(setDeck);
     return () => abortController.abort();
-  }, []);
+  }, [deckId]);
 
+  if ( deck.id === 0 ) return <p>Loading...</p>;
   return (
     <div>
       <nav aria-label="breadcrumb">
@@ -23,6 +25,7 @@ function Study() {
         </ol>
       </nav>
       <h2>Study: {deck.name}</h2>
+      <StudyCard deck={deck} />
     </div>
   );
 }
--- src/Study/StudyCard.js
+++ src/Study/StudyCard.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
-function StudyCard() {
+function StudyCard({ deck }) {
+  const { id, cards } = deck;
+  const history = useHistory();
+
   if (cards.length <= 2) {
     return (
       <div>
         <h3>Not enough cards.</h3>
         <p>You need at least 3 cards to use study mode. There are only {cards.length} cards in this deck.</p>
-        <button type="button" onClick={() => history.push(`/decks/${deck.id}/cards/new`)}>+ Add Cards</button>
+        <button type="button" onClick={() => history.push(`/decks/${id}/cards/new`)}>+ Add Cards</button>
       </div>
     );
   } else {
