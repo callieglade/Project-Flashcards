24183ce6016629c2c164ce7ba46fbbadbc34c221
Callie Glade <callie.glade@meraki.net>
2022-02-02 15:08:53 -0800

    Add button styles and margin spacing

--- src/Deck/Card.js
+++ src/Deck/Card.js
@@ -3,13 +3,13 @@ import React from "react";
 function Card({ card }) {
   return (
     <div className="card">
-      <div className="row">
-        <p className="col">{card.front}</p>
-        <p className="col">{card.back}</p>
+      <div className="row mt-3">
+        <p className="col text-center">{card.front}</p>
+        <p className="col text-center">{card.back}</p>
       </div>
-      <div className="row">
-        <button type="button">Edit</button>
-        <button type="button">Delete</button>
+      <div className="row my-2 mx-2">
+        <button type="button" className="btn btn-secondary mx-3">Edit</button>
+        <button type="button" className="btn btn-danger">Delete</button>
       </div>
     </div>
   );
