f44617a552143cf965877224107948bbe6579338
Callie Glade <callie.glade@meraki.net>
2022-02-03 13:58:01 -0800

    Import and implement Cardform in CardAdd

--- src/Deck/CardAdd.js
+++ src/Deck/CardAdd.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from "react";
-import { useHistory, useParams } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { readDeck, createCard } from "../utils/api";
+import CardForm from "./CardForm";
 
-function CardAdd({ deck, setDeck, isNew }) {
-  const history = useHistory();
+function CardAdd({ deck, setDeck }) {
   const { deckId } = useParams();
 
   useEffect(() => {
@@ -29,54 +29,7 @@ function CardAdd({ deck, setDeck, isNew }) {
   }
 
   return (
-    <div>
-      <nav aria-label="breadcrumb">
-        <ol className="breadcrumb">
-          <li className="breadcrumb-item text-primary" onClick={() => history.push(`/`)}>Home</li>
-          <li className="breadcrumb-item text-primary" onClick={() => history.push(`/decks/${deck.id}`)}>{deck.name}</li>
-          <li className="breadcrumb-item active">{ isNew ? `Add Card` : `Edit Card ${card.id}`}</li>
-        </ol>
-      </nav>
-      <h2>{deck.name}: { isNew ? `Add Card` : `Edit Card` }</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label>Front</label>
-          <textarea
-            id="front"
-            type="text"
-            name="front"
-            className="form-control"
-            placeholder="Front side of card"
-            defaultValue={ isNew ? `` : card.front }
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label>Back</label>
-          <textarea
-            id="back"
-            name="back"
-            className="form-control"
-            placeholder="Back of the card"
-            defaultValue={ isNew ? `` : card.back }
-            required
-          />
-        </div>
-        <div className="row">
-          <button 
-            type="button" 
-            className="btn btn-secondary m-3" 
-            onClick={() => history.push(`/decks/${deck.id}`)}
-            >{ isNew ? `Done` : `Cancel` }
-          </button>
-          <button 
-            type="submit" 
-            className="btn btn-primary my-3"
-            >{ isNew ? `Save` : `Submit` }
-          </button>
-        </div>
-      </form>
-    </div>
+    <CardForm deck={deck} card={{}} isNew={true} handleSubmit={handleSubmit} />
   );
 }
 
