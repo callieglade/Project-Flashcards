d936740e92dffd3efd7b2b6723d1d91b99e29d95
Callie Glade <callie.glade@meraki.net>
2022-02-02 16:10:14 -0800

    Add button links to Edit and Study pages

--- src/Deck/Deck.js
+++ src/Deck/Deck.js
@@ -4,9 +4,6 @@ import Card from "./Card";
 
 function Deck({ deck }) {
   const history = useHistory();
-
-  //if ( deck.id === 0 ) return <p>Loading...</p>;
-
   const cardList = deck.cards.map((card) => (
     <Card key={card.id} card={card} />
   ));
@@ -23,10 +20,28 @@ function Deck({ deck }) {
         <h3>{deck.name}</h3>
         <p>{deck.description}</p>
         <div className="row">
-          <button type="button" className="btn btn-secondary mx-3">Edit</button>
-          <button type="button" className="btn btn-primary mr-3">Study</button>
-          <button type="button" className="btn btn-primary mr-3">+ Add Cards</button>
-          <button type="button" className="btn btn-danger mr-3">Delete</button>
+          <button 
+            type="button" 
+            className="btn btn-secondary mx-3"
+            onClick={() => history.push(`/decks/${deck.id}/edit`)}
+            >Edit
+          </button>
+          <button 
+            type="button" 
+            className="btn btn-primary mr-3"
+            onClick={() => history.push(`/decks/${deck.id}/study`)}
+            >Study
+          </button>
+          <button 
+            type="button" 
+            className="btn btn-primary mr-3"
+            >+ Add Cards
+          </button>
+          <button 
+            type="button" 
+            className="btn btn-danger mr-3"
+            >Delete
+          </button>
         </div>
         <h2 className="my-4">Cards</h2>
         <div>{cardList}</div>
