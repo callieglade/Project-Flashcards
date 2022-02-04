48cd382e44859d7a7111c24d6fb621889027c69d
Callie Glade <callie.glade@meraki.net>
2022-02-03 18:34:02 -0800

    Implement useRouteMatch to clean up Deck route paths

--- src/Deck/index.js
+++ src/Deck/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, useRouteMatch } from "react-router-dom";
 import Deck from "./Deck";
 import Edit from "./Edit";
 import Study from "../Study";
@@ -7,21 +7,23 @@ import CardEdit from "./CardEdit";
 import CardAdd from "./CardAdd";
 
 function DeckLayout({ deck, setDeck }) {
+  const { path } = useRouteMatch();
+
   return (
     <Switch>
-      <Route exact path={`/decks/:deckId`}>
+      <Route exact path={`${path}`}>
         <Deck deck={deck} setDeck={setDeck} />
       </Route>
-      <Route path={`/decks/:deckId/edit`}>
+      <Route path={`${path}/edit`}>
         <Edit deck={deck} setDeck={setDeck} />
       </Route>
-      <Route path={`/decks/:deckId/study`}>
+      <Route path={`${path}/study`}>
         <Study deck={deck} setDeck={setDeck} />
       </Route>
-      <Route path={`/decks/:deckId/cards/new`}>
+      <Route path={`${path}/cards/new`}>
         <CardAdd deck={deck} setDeck={setDeck} isNew={true} />
       </Route>
-      <Route path={`/decks/:deckId/cards/:cardId/edit`}>
+      <Route path={`${path}/cards/:cardId/edit`}>
         <CardEdit deck={deck} setDeck={setDeck} isNew={false} />
       </Route>
     </Switch>
