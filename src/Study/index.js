b24daad0ab91581ed591ac5e9873bb533761b2d9
Callie Glade <callie.glade@meraki.net>
2022-02-03 16:58:00 -0800

    Update conditional rendering for Study to use new initial value for deck

--- src/Study/index.js
+++ src/Study/index.js
@@ -13,20 +13,22 @@ function Study({ deck, setDeck }) {
     return () => abortController.abort();
   }, [deckId, setDeck]);
 
-  if ( deck.id === 0 ) return <p>Loading...</p>;
-  return (
-    <div>
-      <nav aria-label="breadcrumb">
-        <ol className="breadcrumb">
-          <li className="breadcrumb-item text-primary" onClick={() => history.push(`/`)}>Home</li>
-          <li className="breadcrumb-item text-primary" onClick={() => history.push(`/decks/${deckId}`)}>{deck.name}</li>
-          <li className="breadcrumb-item active">Study</li>
-        </ol>
-      </nav>
-      <h2>Study: {deck.name}</h2>
-      <StudyCard deck={deck} />
-    </div>
-  );
+  if (deck.id) {
+    return (
+      <div>
+        <nav aria-label="breadcrumb">
+          <ol className="breadcrumb">
+            <li className="breadcrumb-item text-primary" onClick={() => history.push(`/`)}>Home</li>
+            <li className="breadcrumb-item text-primary" onClick={() => history.push(`/decks/${deckId}`)}>{deck.name}</li>
+            <li className="breadcrumb-item active">Study</li>
+          </ol>
+        </nav>
+        <h2>Study: {deck.name}</h2>
+        <StudyCard deck={deck} />
+      </div>
+    )
+  }
+  return <p>Loading...</p>;
 }
 
 export default Study;
